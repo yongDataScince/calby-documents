40ce74f494ca02db85683674c4d855712d73cb3a
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-06-16 16:12:42 +0300

    [proto-settings] running microservice

--- src/enviroment/mod.rs
+++ src/enviroment/mod.rs
@@ -1,5 +1,10 @@
 use std::{env, net::SocketAddr};
 use std::fmt::Display;
+use crate::service::DocumentsServise;
+
+fn not_provided(var: &str) -> String {
+  format!("variable {} not provided", var)
+}
 
 #[derive(Debug)]
 pub enum Mode {
@@ -15,10 +20,6 @@ impl Display for Mode {
           Development => writeln!(f, "Development")
         }
     }
-}
-
-fn not_provided(var: &str) -> String {
-  format!("variable {} not provided", var)
 }
 
 impl Mode {
@@ -39,6 +40,15 @@ impl Mode {
       };
       (host, port)
     }
+
+    pub fn get_mode() -> Self {
+      match env::var("MODE") {
+        Ok(value) if value == String::from("production") => Self::Production,
+        Ok(value) if value == String::from("development") => Self::Production,
+        Err(_) => panic!("{}", &not_provided("MODE")),
+        Ok(value) => panic!("Unexpected value: {}", value)
+      }
+    }
 }
 
 #[derive(Debug)]
@@ -47,11 +57,31 @@ pub struct Enviroment {
   pub port: String,
   pub format_addr: String,
   pub addr: SocketAddr,
-  pub mode: Mode
+  pub mode: Mode,
+  pub service: DocumentsServise
 }
 
 impl Display for Enviroment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "Server started on address: {}\ndevelopment mode: {}", self.format_addr, self.mode)
     }
+}
+
+impl Enviroment {
+  pub fn init() -> Self {
+    let mode = Mode::get_mode();
+    let (host, port) = mode.get_host_port();
+    let format_addr = format!("http://{}:{}", &host, &port);
+    let addr: SocketAddr = format!("{}:{}", &host, &port).parse().unwrap();
+    let docs_service = DocumentsServise::default();
+
+    Enviroment {
+      service: docs_service,
+      mode,
+      host,
+      port,
+      format_addr,
+      addr
+    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 pub mod data;
 pub mod service;
-
 pub mod enviroment;
 
 extern crate dotenv;
+use documents::documents_server::DocumentsServer;
 use dotenv::dotenv;
+use enviroment::Enviroment;
+use tonic::transport::Server;
 
 pub mod documents {
     tonic::include_proto!("documents");
@@ -13,5 +15,10 @@ pub mod documents {
 #[tokio::main]
 async fn main() {
     dotenv().expect("Please create '.env' file and complete like '.env.example'");
-    println!("Hello, world!");
+
+    let env = Enviroment::init();
+    Server::builder()
+        .add_service(DocumentsServer::new(env.service))
+        .serve(env.addr)
+        .await.expect("cant't run documents service");
 }
