91e3b849dbc11b6cba295a668797ab86a5b152dd
yongDataScince <61432518+yongDataScince@users.noreply.github.com>
2022-06-21 10:31:02 +0300

    [files] init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub mod data;
 pub mod service;
 pub mod enviroment;
+pub mod documents {
+    tonic::include_proto!("documents");
+}
 
 extern crate dotenv;
 use documents::documents_server::DocumentsServer;
@@ -9,35 +12,32 @@ use enviroment::Enviroment;
 use tonic::transport::Server;
 use hyper::service::make_service_fn;
 
-pub mod documents {
-    tonic::include_proto!("documents");
-}
 
-pub async fn shutdown() {
-  tokio::signal::ctrl_c()
-    .await
-    .expect("cant't detect Ctrl+C");
-}
+// pub async fn shutdown() {
+//   tokio::signal::ctrl_c()
+//     .await
+//     .expect("cant't detect Ctrl+C");
+// }
 
 #[tokio::main]
 async fn main() {
     dotenv().expect("Please create '.env' file and complete like '.env.example'");
 
-    let env = Enviroment::init();
-    let service = Server::builder()
-        .add_service(DocumentsServer::new(env.service))
-        .into_service();
+    // let env = Enviroment::init();
+    // let service = Server::builder()
+    //     .add_service(DocumentsServer::new(env.service))
+    //     .into_service();
 
-    let make_service = make_service_fn(move |_conn: &hyper::server::conn::AddrStream| {
-        let service = service.clone();
-        async { Ok::<_, std::convert::Infallible>(service) }
-    });
+    // let make_service = make_service_fn(move |_conn: &hyper::server::conn::AddrStream| {
+    //     let service = service.clone();
+    //     async { Ok::<_, std::convert::Infallible>(service) }
+    // });
 
-    let server = hyper::Server::bind(&env.addr).serve(make_service);
-    println!("\n{}", env);
-    let finisher = server.with_graceful_shutdown(shutdown());
-    match finisher.await {
-      Err(e) => eprintln!("Server error: {}", e),
-       _ => println!("\nServer turn off"),
-    }
+    // let server = hyper::Server::bind(&env.addr).serve(make_service);
+    // println!("\n{}", env);
+    // let finisher = server.with_graceful_shutdown(shutdown());
+    // match finisher.await {
+    //   Err(e) => eprintln!("Server error: {}", e),
+    //    _ => println!("\nServer turn off"),
+    // }
 }
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,11 +1,16 @@
+use std::{fs::File, io::Write};
+use uuid::Uuid;
 use tonic::{
   Request,
   Response,
   Status
 };
 use crate::documents::{
-  documents_server::Documents,
-  SendFilesRequest, SendFilesResponse
+  SaveFilesRequest,
+  SaveFilesResponse,
+  GetFileRequest,
+  GetFileResponse,
+  documents_server::Documents
 };
 
 #[derive(Debug, Default, Clone, Copy)]
@@ -13,16 +18,30 @@ pub struct DocumentsServise;
 
 #[tonic::async_trait]
 impl Documents for DocumentsServise {
-  async fn send_files(
-    &self,
-    request: Request<SendFilesRequest>
-  ) -> Result<Response<SendFilesResponse>, Status> {
-    let req = request.into_inner();
-    let reply = SendFilesResponse {
-      successful: true,
-      message: format!("file name: {}; file type: {}", req.file_name, req.doc_type)
-    };
+    async fn save_files(
+      &self,
+      request: Request<SaveFilesRequest>
+    )
+    -> Result<Response<SaveFilesResponse>, Status> {
+      let req = request.into_inner();
+      let file_id: String = Uuid::parse_str(&format!("{}{}", req.file_name, req.user_id)).expect("error to create uuid").to_string();
 
-    Ok(Response::new(reply))
-  }
+      let reply = SaveFilesResponse {
+        successful: true,
+        file_id: file_id.to_owned(),
+        info_message: format!("Save file with id: {}", file_id.to_owned()),
+      };
+
+      Ok(Response::new(reply))
+    }
+
+    async fn get_file(
+      &self,
+      request: Request<GetFileRequest>
+    ) -> Result<Response<GetFileResponse>, Status> {
+      let req = request.into_inner();
+      todo!()
+    }
 }
+
+
